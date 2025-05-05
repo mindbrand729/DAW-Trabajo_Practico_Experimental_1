f3109e4124f4d7a3e53465f7b0dc39aa05426b2b
Mind Brand Windows <148798600+mindbrand729@users.noreply.github.com>
2025-05-04 21:08:04 -0500

    Correccion de los enlaces de la pagina principal y del .py

--- Sitio_Web_Taller_mecanico/main.py
+++ Sitio_Web_Taller_mecanico/main.py
@@ -6,5 +6,33 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/equipo')
+def equipo():
+    return render_template('equipo.html')
+
+@app.route('/clientes')
+def clientes():
+    return render_template('clientes.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/productos')
+def productos():
+    return render_template('productos.html')
+
+@app.route('/promocion')
+def promocion():
+    return render_template('promocion.html')
+
+@app.route('/sucursales')
+def sucursales():
+    return render_template('sucursales.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
