3bca7aed682723a4759761f25de4409d3c1af0da
al802 <yourname@example.com>
2025-05-09 11:05:13 -0500

    Colocando el usuario y contraseña

---
+++ Sitio_Web_Taller_mecanico/static/js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      const usuario = document.getElementById('username').value;
+      const contrasena = document.getElementById('password').value;
+
+      if (usuario === 'unemi' && contrasena === 'web2025') {
+        alert('Inicio de sesión exitoso');
+      } else {
+        alert('Credenciales incorrectas');
+      }
+    });
